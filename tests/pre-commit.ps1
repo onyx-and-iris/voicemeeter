479e0e1a1935faa6a993ac22dc699144b556ca16
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-06-25 01:16:48 +0100

    add pre-commit

--- tests/pre-commit.ps1
+++ tests/pre-commit.ps1
@@ -1,7 +1,9 @@
 Function RunTests {
-	$run_tests = "cd ../; go clean -testcache; go test ./..."
+	$run_ext_tests = "go clean -testcache; go test -v .\tests\"
+	$run_int_tests = "go clean -testcache; go test -v .\voicemeeter\"
 
-	Invoke-Expression $run_tests
+	Invoke-Expression $run_ext_tests
+	Invoke-Expression $run_int_tests
 }
 
 if ($MyInvocation.InvocationName -ne ".") {
