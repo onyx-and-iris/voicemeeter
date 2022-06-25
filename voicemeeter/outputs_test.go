37a12713f0257e058c8d773ce572a5184b2acc34
onyx-and-iris <75868496+onyx-and-iris@users.noreply.github.com>
2022-06-25 16:20:08 +0100

    add output factory test

---
+++ voicemeeter/outputs_test.go
@@ -0,0 +1,15 @@
+package voicemeeter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetOutputs(t *testing.T) {
+	//t.Skip("skipping test")
+	__o := newOutputs("strip", 0)
+	t.Run("Should return an output type", func(t *testing.T) {
+		assert.NotNil(t, __o)
+	})
+}
